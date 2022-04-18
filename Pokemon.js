1e70bc517626dda29c63a7a1b0c73baacd3938ed
Hunter Baker <96445786+ic3cod3r@users.noreply.github.com>
2022-04-18 11:53:52 -0500

    changed txt

--- Pokemon.js
+++ Pokemon.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Pokemon = () => {
     return (
-        <p>Type in a pokemon to get started</p>
+        <><p>Type in a pokemon to get started</p><p>Here are some pokemon below, type in a certain pokemon in the box above to get more in depth detail</p></>
     )
 }
 
